67a6c4331e027a5bb5beaedbcb1ab24d8ce8f41c
Pedro Henrique De Lima <79190116+Pedroo13@users.noreply.github.com>
2022-08-31 10:41:41 -0300

    Add files via upload

---
+++ orientacaoObjeto2/testeAbc.py
@@ -0,0 +1,8 @@
+from abc import ABC # abstract base classes
+from collections.abc import MutableSequence
+
+class MinhaListinhaMutavel(MutableSequence):
+    pass
+
+objetoValidado = MinhaListinhaMutavel()
+print(objetoValidado)
